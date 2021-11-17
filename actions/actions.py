ccb4315164d5dc54d8f8c8a48564cef0d8293809
Andrew Huang (Cheng Chiang) <33182390+epadam@users.noreply.github.com>
2021-11-17 04:19:22 +0100

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import Text, Dict, Any, List, Union
 import os
+import requests
 import logging
 from dateutil import parser
 import sqlalchemy as sa
@@ -21,9 +22,32 @@ from rasa_sdk.events import (
     UserUtteranceReverted,
 )
 
-
-
 logger = logging.getLogger(__name__)
+
+class ActionHaystack(Action):
+
+    def name(self) -> Text:
+        return "call_haystack"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        url = "http://localhost:8000/query"
+        payload = {"query": str(tracker.latest_message["text"])}
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", url, headers=headers, json=payload).json()
+
+        if response["answers"]:
+            answer = response["answers"][0]["answer"]
+        else:
+            answer = "No Answer Found!"
+
+        dispatcher.utter_message(text=answer)
+
+        return []
 
 
 
